a36d1e88fe69dfbde8767745dfb4db48014a7ca8
AS-DEVELOPER <67372463+AS-DEVELOPER7@users.noreply.github.com>
2022-11-15 14:30:41 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,28 @@ import { Provider } from "react-redux";
 import { store } from "../app/store";
 import StateContext from "../context/StateContext";
 import Layout from "../components/Layout";
+import { useState } from "react";
+import { FaSpotify } from "react-icons/fa";
+import { useEffect } from "react";
 function MyApp({ Component, pageProps }) {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
   return (
     <Provider store={store}>
       <StateContext>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        {loading ? (
+          <div className="bg-black flex items-center justify-center w-[100vw] h-[100vh]">
+            <FaSpotify className="text-green-500 h-[8em] w-[8em]" />
+          </div>
+        ) : (
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        )}
       </StateContext>
     </Provider>
   );
